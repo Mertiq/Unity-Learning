1f4fe3a5429e3998f5107829d4e6ad60974533bd
Mert <mertiq@icloud.com>
2020-11-10 22:00:45 +0300

    unity

---
+++ 3D/Assets/Camera Shake/Scripts/MouseClicked.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class MouseClicked : MonoBehaviour
+{
+    public CameraShake cs;
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartCoroutine(cs.Shake(.15f, .4f));
+        }
+    }
+}
---
+++ 3D/Assets/NavMesh/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    public Camera cam;
+
+    public NavMeshAgent agent;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+}
