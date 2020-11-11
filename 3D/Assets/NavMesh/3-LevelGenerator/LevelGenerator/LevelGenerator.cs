ce0c6b36a3ecff288e474ff6bff7f754e3e88d48
Mert İshak Kılıç <61623447+Mertiq@users.noreply.github.com>
2020-11-11 17:21:04 +0300

    Add files via upload

---
+++ 3D/Assets/NavMesh/3-LevelGenerator/LevelGenerator/LevelGenerator.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class LevelGenerator : MonoBehaviour {
+
+	public NavMeshSurface surface;
+
+	public int width = 10;
+	public int height = 10;
+
+	public GameObject wall;
+	public GameObject player;
+
+	private bool playerSpawned = false;
+
+	// Use this for initialization
+	void Start () {
+		GenerateLevel();
+
+		surface.BuildNavMesh();
+	}
+	
+	// Create a grid based level
+	void GenerateLevel()
+	{
+		// Loop over the grid
+		for (int x = 0; x <= width; x+=2)
+		{
+			for (int y = 0; y <= height; y+=2)
+			{
+				// Should we place a wall?
+				if (Random.value > .7f)
+				{
+					// Spawn a wall
+					Vector3 pos = new Vector3(x - width / 2f, 1f, y - height / 2f);
+					Instantiate(wall, pos, Quaternion.identity, transform);
+				} else if (!playerSpawned) // Should we spawn a player?
+				{
+					// Spawn the player
+					Vector3 pos = new Vector3(x - width / 2f, 1.25f, y - height / 2f);
+					Instantiate(player, pos, Quaternion.identity);
+					playerSpawned = true;
+				}
+			}
+		}
+	}
+
+}
