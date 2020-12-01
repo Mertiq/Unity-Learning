e681aa666e247de4725e880bee3d527619c28380
Mert <mertiq@icloud.com>
2020-12-02 00:27:14 +0300

    slowtime-rts

---
+++ 2D/Assets/RTS CAMERA/RtsCamera.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RtsCamera : MonoBehaviour
+{
+    public Camera cam;
+
+    public float panSpeed = 20f;
+    public float panBorderThickness = 10f;
+
+    public Vector2 panLimit;
+    public float maxSize;
+    public float minSize;
+
+    public float scrollSpeed = 20f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+
+        if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness)
+        {
+            pos.y += panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness)
+        {
+            pos.y -= panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorderThickness)
+        {
+            pos.x += panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("a") || Input.mousePosition.x <= panBorderThickness)
+        {
+            pos.x -= panSpeed * Time.deltaTime;
+        }
+
+        float scroll =  Input.GetAxis("Mouse ScrollWheel");
+        float size = cam.orthographicSize;
+        size -= scroll * scrollSpeed * 1000f * Time.deltaTime;
+        cam.orthographicSize = Mathf.Clamp(size, minSize, maxSize);
+
+        pos.x = Mathf.Clamp(pos.x, -panLimit.x, panLimit.x);
+        pos.y = Mathf.Clamp(pos.y, -panLimit.y, panLimit.y);
+        
+        
+        transform.position = pos;
+    }
+}
---
+++ 2D/Assets/Slow Motion/TimeManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+    public float slowdownFactor = 0.05f;
+    public float slowdownLength = 2f;
+
+    void DoSlowMotion()
+    {
+        Time.timeScale = slowdownFactor;
+        Time.fixedDeltaTime = Time.timeScale * .02f;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            DoSlowMotion();
+        }
+        Time.timeScale += (1f / slowdownLength )* Time.unscaledDeltaTime;
+        Time.timeScale = Mathf.Clamp(Time.timeScale, 0f, 1f);
+    }
+}
